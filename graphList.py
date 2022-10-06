2b2a5aa1806e8c6197ede6e75f25d60604f2a463
Seng Sovanpanhavath <114840775+sovanpanhavathseng@users.noreply.github.com>
2022-10-06 19:54:47 +0700

    Create graphList.py

---
+++ graphList.py
@@ -0,0 +1,49 @@
+graph = {}
+print(graph)
+
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1944
+}
+
+car.clear()
+print(car)
+
+a = car.copy()
+print(a)
+
+b = ('key1',' key2', 'key3')
+c = 0
+d = graph.fromkeys(b, c)
+print(d)
+
+e = car.get("model")
+print(e)
+
+f = car.items()
+print(f)
+
+g = car.keys()
+print(g)
+
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1944
+}
+
+car.pop("model")
+print(car)
+
+car.popitem()
+print(car)
+
+h = car.setdefault("model", "Bronco")
+print(h)
+
+car.update({"Number": "6"})
+print(car)
+
+i = car.values()
+print(i)
