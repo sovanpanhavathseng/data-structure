e85554e6d1ed14330d974ce921e5eec69d8561e6
Seng Sovanpanhavath <114840775+sovanpanhavathseng@users.noreply.github.com>
2022-10-09 11:22:55 +0700

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,40 @@
+myTuple = (12, 34, 56)
+print(myTuple)
+
+
+a = ('yellow', 'Green', 'Blue')
+print(a)
+
+
+b = (1, 2, 3, 1, 3)
+print(b)
+
+
+c = (6, 7, 8)
+print(len(c))
+
+
+d = (1,)
+print(type(d))
+
+#NOT a tuple
+d = (1)
+print(type(d))
+
+
+tuple1 = ('a', 'b', 'c')
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+print(tuple1, tuple2, tuple3)
+
+
+tuple1 = ('abc', 34, True, 40, 'male')
+print(tuple1)
+
+
+myTuple = ('d', 'e', 'f')
+print(type(myTuple))
+
+
+e = tuple(('apple', 'ball', 'car'))# note the double round-breakpoint
+print(e)
