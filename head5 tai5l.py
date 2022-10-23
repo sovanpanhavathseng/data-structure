8f39dbe42cb6f99d7ffde0355717672b83fae4cc
Seng Sovanpanhavath <114840775+sovanpanhavathseng@users.noreply.github.com>
2022-10-23 09:41:38 +0700

    Create head5 tai5l.py

---
+++ head5 tai5l.py
@@ -0,0 +1,63 @@
+def tail(n):
+    if n > 0:#Base case
+        print(n,end="")
+        tail(n-1)#recursive step
+tail(1)
+
+def tail(i):
+    if i > 0:#Base case
+        print(i,end="")
+        tail(i-1)#recursive step
+tail(2)
+
+def tail(j):
+    if j > 0:#Base case
+        print(j,end="")
+        tail(j-1)#recursive step
+tail(3)
+
+def tail(k):
+    if k > 0:#Base case
+        print(k,end="")
+        tail(k-1)#recursive step
+tail(4)
+
+def tail(l):
+    if l > 0:#Base case
+        print(l,end="")
+        tail(l-1)#recursive step
+tail(5)
+
+
+
+
+
+def head(m):
+    if m > 0:#Base case
+        head(m-1)
+        print(m,end="")
+head(1)
+
+def head(o):
+    if o > 0:#Base case
+        head(o-1)
+        print(o,end="")
+head(2)
+
+def head(p):
+    if p > 0:#Base case
+        head(p-1)
+        print(p,end="")
+head(3)
+
+def head(q):
+    if q > 0:#Base case
+        head(q-1)
+        print(q,end="")
+head(4)
+
+def head(r):
+    if r > 0:#Base case
+        head(r-1)
+        print(r,end="")
+head(5)
