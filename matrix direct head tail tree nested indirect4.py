98c02416edf85105c8294f0612e059204a8bae81
Seng Sovanpanhavath <114840775+sovanpanhavathseng@users.noreply.github.com>
2022-12-29 08:09:27 +0700

    Create matrix direct head tail tree nested indirect4.py

---
+++ matrix direct head tail tree nested indirect4.py
@@ -0,0 +1,53 @@
+graph = [[]]
+print(graph)
+
+graph.append(100000000000000000000000000000000000000000000000000000)
+print(graph)
+
+def direct(graph):
+    if graph == 1:#Base case
+        return 1
+    else:
+        return graph * direct(graph-1)
+print(direct(5))
+
+
+def tail(b):
+    if b > 0:#Base case
+        print(b,end="")
+        tail(b-1)#resive step
+tail(5)
+
+
+def head(c):
+    if c > 0:#Base case
+        head(c-1)#recursive step
+        print(c,end="")
+head(10)
+
+
+def tree(d):
+    if d > 0:#Base case
+        tree(d-1)#recursive step
+        tree(d-1)#recursive step
+tree(15)
+
+
+def nested(e):
+    if e > 100:#Base case
+        return e-10
+    return nested(nested(e+11))
+nested(20)
+
+
+def A(n):
+    if n > 0:#Base case
+        print(n,end="")
+        # A is colling B
+        B(n-1)#recursive step
+def B(n):
+    if n > 1:#Base case
+        print(n,end="")
+        # B colling A
+        A(n//1)#recursive step
+A(20)
